1d54c9d702afd3dc6767a7337c30f1bc85aea8fa
anupDEVpattnaik <anup24091@iiitd.ac.in>
2025-11-15 12:23:03 +0530

    Add new folder with 3 files

---
+++ database/auth_schema.sql
@@ -0,0 +1,20 @@
+DROP DATABASE IF EXISTS auth_db;
+CREATE DATABASE auth_db;
+USE auth_db;
+
+CREATE TABLE users_auth (
+    user_id int AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    username varchar(50) NOT NULL,
+    role enum('student','instructor','admin') NOT NULL,
+    password_hash varchar(255) NOT NULL,
+    status enum('active','inactive') DEFAULT 'active',
+    last_login datetime DEFAULT NULL,
+)
+
+CREATE TABLE password_history (
+    log_id int AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    user_id int NOT NULL,
+    password_hash varchar(255) NOT NULL,
+    changed_at datetime DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users_auth(user_id)
+);
---
+++ database/schema.sql
@@ -0,0 +1,60 @@
+DROP DATABASE IF EXISTS erp_db;
+CREATE DATABASE erp_db;
+USE erp_db;
+
+CREATE TABLE courses (
+    code char(6) PRIMARY KEY,
+    title varchar(100) NOT NULL,
+    credits int 
+);
+
+CREATE TABLE students (
+    user_id int PRIMARY KEY,
+    roll_no varchar(7) NOT NULL,
+    program enum('B.Tech','M.Tech') NOT NULL,
+    year int NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES auth_db.users_auth(user_id)
+);
+
+CREATE TABLE instructors (
+    user_id int PRIMARY KEY,
+    department varchar(100) NOT NULL,
+    FOREIGN KEY (`user_id`) REFERENCES `auth_db`.`users_auth` (`user_id`)
+);
+
+CREATE TABLE sections (
+    section_id int AUTO_INCREMENT PRIMARY KEY,
+    course_id varchar(6) NOT NULL,
+    instructor_id int NOT NULL,
+    day_time varchar(50),
+    room varchar(20),
+    capacity int,
+    semester enum('Monsoon','Winter','Summer') NOT NULL,
+    year year NOT NULL,
+    FOREIGN KEY (course_id) REFERENCES courses(code),
+    FOREIGN KEY (instructor_id) REFERENCES instructors(user_id)
+);
+
+CREATE TABLE enrollments (
+    enrollment_id int AUTO_INCREMENT PRIMARY KEY,
+    student_id int NOT NULL,
+    section_id int NOT NULL,
+    status enum('enrolled','dropped') DEFAULT 'enrolled',
+    UNIQUE(student_id,section_id),
+    FOREIGN KEY (student_id) REFERENCES students(user_id),
+    FOREIGN KEY (section_id) REFERENCES sections(section_id)
+);
+
+CREATE TABLE grades (
+    grade_id int AUTO_INCREMENT PRIMARY KEY,
+    enrollment_id int NOT NULL,
+    component varchar(30) NOT NULL,
+    score decimal(5,2) CHECK ((`score` >= 0)) ,
+    final_grade char(2),
+    FOREIGN KEY (`enrollment_id`) REFERENCES `enrollments` (`enrollment_id`)
+);
+
+CREATE TABLE settings (
+    setting_key varchar(50) PRIMARY KEY,
+    setting_value varchar(50) NOT NULL
+);
